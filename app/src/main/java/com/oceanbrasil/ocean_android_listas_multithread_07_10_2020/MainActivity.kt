09e86503147c5a4c059fdc6a4e5f78051634fc79
Paulo Salvatore <paulo@salvatore.tech>
2020-10-07 18:43:06 -0300

    Initial commit

---
+++ app/src/main/java/com/oceanbrasil/ocean_android_listas_multithread_07_10_2020/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.oceanbrasil.ocean_android_listas_multithread_07_10_2020
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
